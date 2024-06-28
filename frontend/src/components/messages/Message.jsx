98969f6dca77cc75d546e41d012730825d1a9817
anuragshukla28 <anuragshukla0728@gmail.com>
2024-06-29 00:32:34 +0530

    2nd part completed ui design part

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -3,7 +3,10 @@ const Message = () => {
         <div className="chat chat-end">
             <div className="chat-image avatar">
                 <div className="w-10 rounded-full">
-
+                <img
+							src='https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png'
+							alt='user avatar'
+						/>
 
                 </div>
             </div>

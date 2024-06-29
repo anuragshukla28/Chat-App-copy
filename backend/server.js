0e9c83028a5c21390cf0ea611c9e9f95fdeb51b4
anuragshukla28 <anuragshukla0728@gmail.com>
2024-06-29 18:14:55 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,6 +9,8 @@ import userRoutes from"./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
+
+const __dirname = path.resolve();
 const PORT= process.env.PORT || 5001;
 
 dotenv.config();
@@ -19,10 +21,11 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/" ,(req , res) =>{
-// //root route http://localhost:5001/
-// res.send("hello World !!!");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});  
 
 
 
